ccbfe900c380b0e10ee3ab7ceb560c90588a5626
Fedchenko Artem <artem_fedchenko_2017@mail.ru>
2024-11-06 20:25:24 +0700

    feat(period-btns): add outline for selected btn

--- src/app/ui/time-period-btns/time-period-btns.component.ts
+++ src/app/ui/time-period-btns/time-period-btns.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TimePeriod } from '../../services/chart-time-period/time-period.model';
 import { TimePeriodService } from '../../services/chart-time-period/time-period.service';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-time-period-btns',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './time-period-btns.component.html',
   styleUrl: './time-period-btns.component.scss',
 })
@@ -20,5 +21,9 @@ export class TimePeriodBtnsComponent {
 
   public changePeriod(period: TimePeriod) {
     this.timePeriodService.setTimePeriod(period);
+  }
+
+  public isCurrentPeriod(period: TimePeriod) {
+    return this.timePeriodService.period === period;
   }
 }
